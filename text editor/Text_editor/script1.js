f712b6c02630e6e1e4feab53ee8280113dd2fd48
HafssaELfathi <hafssaelfathi4@gmail.com>
2026-09-18 22:49:08 +0100

    create text editor project

---
+++ text editor/Text_editor/script1.js
@@ -0,0 +1,31 @@
+function editoroption1() {
+    document.getElementById("textarea1").style.fontWeight = "bold";
+}
+function editoroption2() {
+    document.getElementById("textarea1").style.fontStyle = "italic";
+}
+function editoroption3() {
+    document.getElementById("textarea1").style.textAlign = "left";
+} 
+function editoroption4() {
+    document.getElementById("textarea1").style.textAlign = "center";
+}
+function editoroption5() {
+    document.getElementById("textarea1").style.textAlign = "right";
+}
+function editoroption6() {
+    document.getElementById("textarea1").style.textTransform = "uppercase";
+}   
+function editoroption7() {
+    document.getElementById("textarea1").style.textTransform = "lowercase";
+}
+function editoroption8() {
+    document.getElementById("textarea1").style.textTransform = "capitalize";
+}
+function editoroption9() {
+    document.getElementById("textarea1").style.fontWeight = "normal";
+    document.getElementById("textarea1").style.fontalign = "left";
+    document.getElementById("textarea1").style.fontStyle = "normal";
+    document.getElementById("textarea1").style.textTransform = "none";
+    document.getElementById("textarea1").value = "";//clears the text area
+}
